85ae48414206ca75e9db09861c9863a1842e0114
github username <github email address>
2018-10-09 09:53:46 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+  // props.onReceiveCoordinates is a function from index.js
+  // it just logs the coordinates
+  render() {
+    return (
+      <button onClick={this.makeArray}></button>
+    )
+  }
+  makeArray = (event) => {
+    event.persist()
+    const array = [event.clientX, event.clientY]
+    this.props.onReceiveCoordinates(array)
+  }
+}
+
+// REMEMBER THE USE OF ARROW FUNCTIONS!
