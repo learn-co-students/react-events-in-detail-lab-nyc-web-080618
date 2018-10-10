25278a9aa0b074cae58266759f60c890eb5f2637
ParkMD7 <parkermdinsmore@gmail.com>
2018-10-09 22:40:08 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,34 @@
-// Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+// Deliverable #1 & #2
+// In the components/CoordinatesButton.js file, create a CoordinatesButton React component.
+// This component takes in one prop: onReceiveCoordinates. This prop is a function passed down
+// from index.js. This function is currently just logging whatever is passed into it.
+export default class CoordinatesButton extends Component {
+    constructor(props){
+      super(props)
+      console.log(props);
+      // verified return of props
+    }
+
+  // Deliverable #3
+  // Within CoordinatesButton, render a button. On click of the button, create an array with
+  // two elements: the X and Y coordinates of the mouse. Find these using the event data.
+  render(){
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click This My Guy</button>
+      </div>
+    )
+  }
+
+  // Deliverable #4
+  // Pass this event data in as the argument for the onReceiveCoordinates prop.
+  // If successful, the current x,y position of your mouse should be logged.
+  handleClick = (event) => {
+    console.log("logging clientX ", event.clientX);
+    console.log("logging clientY ", event.clientY);
+    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+  }
+
+} // end of CoordinatesButton Component
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,32 @@
-// Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+// Deliverable #1 & #2
+// In the components/DelayedButton.js file, create a DelayedButton React component
+// This component takes two props: onDelayedClick (a function), and delay (a number).
+export default class DelayedButton extends Component {
+  constructor(props){
+    super(props)
+    console.log("Logging props from inside DelayedButton ", props);
+    // verified return of both expected props
+  }
+
+  // Deliverable #3
+  // Create a button that, when clicked, will pass the click event to the onDelayedClick
+  // prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+  render(){
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me (Delayed)</button>
+      </div>
+    )
+  }
+
+  // Deliverable #4
+  // If successful, the event will be logged to the console once the timeout has finished.
+  handleClick = (event) => {
+    event.persist()
+    setTimeout(() => { this.props.onDelayedClick(event) }, this.props.delay);
+    console.log(event);
+  }
+
+} // end of DelayedButton Component
